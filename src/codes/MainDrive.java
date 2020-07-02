31551f491b52f51a048648ce4b30648a80f94ce2
Lee-Jae-Woong <67721149+Lee-Jae-Woong@users.noreply.github.com>
2020-07-02 14:46:35 +0900

    전화번호 목록 파일에서 불러오기

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,5 +1,12 @@
 package codes;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class MainDrive {
@@ -41,12 +48,15 @@ public class MainDrive {
 			}
 			
 			else if (inputMenu == 1) {
-//				전화번호 추가 기능 구현 필요
+				
+//				전화번호 추가 기능 실행
+				addUserInfoToFile();
 			}
 			
 			else if (inputMenu == 2) {
 //				전화번호 전체 조회 기능
 				
+				readAllPhoneNum();
 			}
 				
 			else {
@@ -59,6 +69,79 @@ public class MainDrive {
 		}
 		
 	}
+
+// 파일에 저장된 전화번호 목록 출력
+	
+	public static void readAllPhoneNum() {
+
+//	파일에 저장된 데이터를 => 자바 프로그램에 활용 (File Input)
+//		File Reader / BufferedReader 활용
+		
+//		불러올 파일의 위치 지정  => 저장할때 사용하는 파일명과 동일하게.
+		
+		File file =new File("phoneBook.txt");
+		
+//		파일을 실제로 불러오는 클래스
+		
+		try {
+			
+			FileReader fr = new FileReader(file);
+			
+			
+//			한줄씩 한꺼번에 불러오게 하는 클래스 => fr은 한글자씩. fr을 보조해서 한문장 로드
+			BufferedReader br = new BufferedReader(fr);
+			
+//			모든 연락처를 불러올때까지 반복
+			
+			while (true) {
+//				
+//				한줄을 통째로 불러오기
+				String line = br.readLine();
+				
+				
+				if(line == null) {
+					
+//					떠이상 불러올 내용이 없어서 null이 들어옴
+//					=> 다 읽었다. => 무한반복 탈출
+					
+					System.out.println("연락처를 모두 불러왔습니다.");
+					break;
+					
+				}
+				
+//				이 줄의 코드가 실행된다 : break를 안만났다. => 불러온 내용이 null이 아니다.
+				
+				System.out.println(line);
+				
+			}
+			
+//			while 빠져나옴 : 파일을 다 읽었으니 빠져나왔다. ㅎㅎ
+//			파일 사용을 종료. br부터 닫고 fr을 닫자
+			
+			br.close();
+			fr.close();
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			
+//			읽어올 파일이 없는 경우. (삭제 or 아직 안만든 경우)
+			
+			System.out.println("불러올 파일이 없습니다");
+			System.out.println("연락처를 저장하고 다시 시도해주세요.");
+			
+			
+			
+//			e.printStackTrace();
+		} catch (IOException e) {
+			
+			System.out.println("연락처를 읽어오는중에 문제가 발생했다.");
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
 	
 //	전화번호 + 이름 + 생년 정보 저장기능.
 	public static void addUserInfoToFile() {
@@ -80,6 +163,41 @@ public class MainDrive {
 		
 //		변수에 저장한 데이터를 묶어서 파일로 저장.
 //		java => 보조기억장치 내보내기 => 파일 출력
+		
+//		어느 파일을 이용할지 파일명 설정
+		File phoneBookFile = new File("phoneBook.txt");
+		
+		try {
+			FileWriter fw = new FileWriter(phoneBookFile, true);
+			
+//			fw는 개발자가 다루기 불편함. => 보조도구 : bw
+			
+			BufferedWriter bw = new BufferedWriter(fw);
+			
+//			실제로 bw를 이용해서 연락처 정보 저장.
+//			저장내용 : 3가지 정보를 한 줄로 묶어서.
+//			ex :  조경진, 010-0000-0000, 2000
+			
+			String infoStr = String.format("%s, %s, %d", name, phoneNum, birthYear);
+			
+//			묶인 한 줄을 파일에 기록
+			bw.append(infoStr);
+//			파일에 기록하고 나면, 줄이 바뀌지 않는다. => System.out.print 처럼 동작
+			bw.newLine();
+			
+//			작업완료=> 열어둔 bw, fw를 닫아주자
+			
+			bw.close();
+			fw.close();
+			
+			System.out.println("연락처 저장이 완료되었습니다.");
+			
+			
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 		
 		
 	}
